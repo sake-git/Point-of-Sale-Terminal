d3f6389658e83cdbf6cc383a42fe5851e9076478
Shweta Karne <ShwetaKarne84@gmail.com>
2024-12-12 15:41:24 -0800

    initial message

---
+++ Point of Sale/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Point_of_Sale
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
