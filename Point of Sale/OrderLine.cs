c45a7547a3f18e9430ddde5e628691afee6a7e55
Mazen Saber <mazensaber1996@gmail.com>
2024-12-13 06:46:02 -0500

    Added OrderLine and Order Classes

---
+++ Point of Sale/OrderLine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point_of_Sale
+{
+    internal class OrderLine
+    {
+        public Product Item { get; set; }
+        public int Quantity { get; set; }
+        public double OrderLinePrice { get; set; }
+
+        public OrderLine(Product product, int quantity)
+        {
+            Item = product;
+            Quantity = quantity;
+            OrderLinePrice = product.Price * quantity;
+        }
+    }
+}
