0bd024a20cd10fff54998c81d54721621a6a8331
rebeccah-barlas <rebeccah.m.barlas@gmail.com>
2024-12-13 12:57:12 -0500

    first commit

--- Point of Sale/Program.cs
+++ Point of Sale/Program.cs
@@ -1,10 +1,11 @@
-﻿namespace Point_of_Sale
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿using Point_of_Sale;
+
+Console.WriteLine("Here are the available products:");
+Console.WriteLine(Product.DisplayAllProducts());
+
+Console.WriteLine("Which product would you like to purchase? Enter 1-12");
+int userInput = int.Parse(Console.ReadLine());
+string userChoice = Product.DisplayProduct(userInput - 1);
+Console.WriteLine(userChoice);
+
+
