89742a32afcf12e5e5ef0c5df4f5ae487f1f791d
Shweta Karne <ShwetaKarne84@gmail.com>
2024-12-16 16:49:36 -0800

    Added Program.cs file

---
+++ Point of Sale/Program.cs
@@ -0,0 +1,71 @@
+﻿using Point_of_Sale.Banking;
+using Point_of_Sale.ErrorLogging;
+
+namespace Point_of_Sale
+{
+    public enum paymentMethod
+    {
+        Cash = 1,
+        Check,
+        Credit
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool isPaid = false;
+            string input;
+            //FileOperations.ReadProductsFromFile("Product.txt");
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Enter Amount");
+                    double amount = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("How would you like to pay today?");
+                    foreach (string name in Enum.GetNames(typeof(paymentMethod)))
+                    {
+                        Console.WriteLine($"{(int)Enum.Parse(typeof(paymentMethod), name)}. {name}");
+                    }
+                    // Console.WriteLine("\n1. Cash \n2. Check \n3. Credit Card");
+                    string method = Console.ReadLine();
+                    paymentMethod enumvalue;
+                    Enum.TryParse(method, out enumvalue);
+
+                    string message = "";
+
+                    switch ((int)enumvalue)
+                    {
+                        case 1:
+                            message = Cash.Payment(amount);
+                            break;
+                        case 2:
+                        case 3:
+                            message = Bank.PaymentGateway(amount, (int)enumvalue);
+                            break;
+
+                        default:
+                            throw new InvalidDataException("Invalid Payment Method!");
+                            break;
+                    }
+
+                    isPaid = true;
+                    Console.WriteLine("Payment Accepted!");
+                    Console.WriteLine(message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Transaction Declined!");
+                    Logger.LogError(ex);
+                    isPaid = false;
+                }
+                Console.WriteLine("Would you like to continue? (y/n)");
+                input = Console.ReadLine();
+            } while (input.ToLower() == "y");
+
+            //FileOperations.SaveProductsToFile("Product.txt"); 
+        }
+    }
+}
